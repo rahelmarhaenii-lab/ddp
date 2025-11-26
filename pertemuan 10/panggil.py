3bcdce07989888eddfdf0393015fe3cab4c1dbe2
rahelmarhaenii-lab <rahelmarhaenii@gmail.com>
2025-11-26 11:12:38 +0700

    Add files via upload

---
+++ pertemuan 10/panggil.py
@@ -0,0 +1,21 @@
+import BangunRuang as BR
+import BangunDatar as BD
+
+print("--- Bangun Ruang ---")
+print(BR.kubus(9))
+print(BR.balok(2,3,4))
+print(BR.prisma(10,24,15))
+print(BR.tabung(2,5))
+print(BR.kerucut(3,5))
+
+print("--- Bangun Datar ---")
+print(BD.persegi(4))
+print(BD.kel_persegi(5))
+print(BD.persegi_panjang(6,7))
+print(BD.kel_persegi_panjang(2,3))
+print(BD.lingkaran(1))
+print(BD.kel_lingkaran(2))
+print(BD.segitiga(6,7))
+print(BD.kel_segitiga(14,16,17))
+print(BD.jajargenjang(4,5))
+print(BD.kel_jajargenjang(3,4))
